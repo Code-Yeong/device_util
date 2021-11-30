b0498e4136dd03b9755a75c1232132586d431110
yeongguo <yeongguo@tencent.com>
2021-11-30 21:54:22 +0800

    取消windows动态dll库

--- windows/include/device_util/device_util_plugin.h
+++ windows/include/device_util/device_util_plugin.h
@@ -3,17 +3,11 @@
 
 #include <flutter_plugin_registrar.h>
 
-#ifdef FLUTTER_PLUGIN_IMPL
-#define FLUTTER_PLUGIN_EXPORT __declspec(dllexport)
-#else
-#define FLUTTER_PLUGIN_EXPORT __declspec(dllimport)
-#endif
-
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
-FLUTTER_PLUGIN_EXPORT void DeviceUtilPluginRegisterWithRegistrar(
+void DeviceUtilPluginRegisterWithRegistrar(
     FlutterDesktopPluginRegistrarRef registrar);
 
 #if defined(__cplusplus)
